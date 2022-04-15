3715167082949b52a051dd30b4c729c7202cf71f
Miha <mihalo3c@gmail.com>
2022-04-15 10:36:41 +0200

    add approval logic

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,77 @@ require('hardhat-deploy-ethers');
 require('hardhat-abi-exporter');
 require('hardhat-deploy');
 
-// TODO: Add task to approve tokens 
-// const { task } = require("hardhat/config");
+const { task } = require("hardhat/config");
+
+task("approve-token", "Approves ERC20 token for spender (max approval only)")
+  .addParam("token", "ERC20 token address")
+  .addParam("spender", "Address of the spender")
+  .setAction(
+    async({token, spender}, hre) => {
+      const { approveERC20, getERC20Allowance } = require('./src/test/helpers')
+      
+      const maxUint = hre.ethers.constants.MaxUint256
+      const { trader: traderAdd } = await hre.getNamedAccounts()
+      const trader = await hre.ethers.getSigner(traderAdd)
+      console.log(`Approving spender ${spender} to spend ${token} of ${trader.address}`)
+
+      await approveERC20(
+          trader, 
+          token, 
+          spender, 
+          maxUint
+      )
+      const allowance = await getERC20Allowance(
+          trader, 
+          token, 
+          trader.address,
+          spender
+      )
+      if (allowance.lt(maxUint)) {
+          throw new Error(`${token} allowance is not maxUint`)
+      }
+    }
+  )
+task("approve-all", "Approves ERC20 tokens")
+  .setAction(
+    async({}, hre) => {
+      const { approveERC20, getERC20Allowance } = require('./src/test/helpers')
+      const maxUint = hre.ethers.constants.MaxUint256
+      const { trader: traderAdd } = await hre.getNamedAccounts()
+      const trader = await hre.ethers.getSigner(traderAdd)
+
+      const chainID = await hre.getChainId()
+      const tokens = require('./src/static/instructions/tokens.json')
+        .filter(t => t.chainID == chainID)
+        .map(t => t.id)
+      let spender
+      if (chainID == getEnvVar('DFK_CHAIN_ID', true)) {
+        spender = require('./src/static/dexes.json').find(d => d.id == 'dfk-swap').router
+      } else if (chainID == getEnvVar('AVAX_CHAIN_ID', true)) {
+        spender = require('./src/static/config.json').yakrouter
+      } else {
+        throw new Error(`Unsupported chainID: ${chainID}`)
+      }
+
+      for (let token of tokens) {
+        const allowance = await getERC20Allowance(
+          trader, 
+          token, 
+          trader.address,
+          spender
+        )
+        if (allowance.lt(maxUint)) {
+          console.log(`Approving spender ${spender} to spend ${token} of ${trader.address}`)
+          await approveERC20(
+              trader, 
+              token, 
+              spender, 
+              maxUint
+          )
+        }
+      }
+    }
+  )
 
 const { getEnvVar } = require('./src/utils/utils');
 
@@ -43,7 +112,7 @@ module.exports = {
         count: 20
       }
     }, 
-    mainnetAVAX: {
+    'avax-mainnet': {
       chainId: 43114,
       gasPrice: 225000000000,
       url: getEnvVar('AVAX_RPC'),
@@ -51,9 +120,9 @@ module.exports = {
         getEnvVar('PK_LIVE_43114')
       ]
     },
-    mainnetDFK: {
+    'dfk-mainnet': {
       chainId: 53935,
-      gasPrice: 1,
+      gasPrice: 1000000000,
       url: getEnvVar('DFK_RPC'),
       accounts: [
         getEnvVar('PK_LIVE_53935')
